392c9fb9932921e7e99139f8ea2a83c20c59e517
Ivan D. Velez <ivancho175@hotmail.com>
2021-09-21 14:51:44 -0500

    First commit of Lucro-inside's project. Adding initial assets and settings. First view (fullHD Desktop) was implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { PurposeComponent } from './components/purpose/purpose.component';
+import { BeliefsComponent } from './components/beliefs/beliefs.component';
+import { WorkComponent } from './components/work/work.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PurposeComponent,
+    BeliefsComponent,
+    WorkComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/beliefs/beliefs.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BeliefsComponent } from './beliefs.component';
+
+describe('BeliefsComponent', () => {
+  let component: BeliefsComponent;
+  let fixture: ComponentFixture<BeliefsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BeliefsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BeliefsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
