68170dfbda037d37a652e777ca426c18d75b5334
Ivan D. Velez <ivancho175@hotmail.com>
2021-10-04 11:30:18 -0500

    first commit on branch 'developer'. creating 'Empresas' and 'Contactenos' components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { EmpresasComponent } from './pages/empresas/empresas.component';
+
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'empresas',
+    redirectTo: 'EmpresasComponent',
+  },
+  {
+    path: 'contactenos',
+    redirectTo: 'ContactenosComponent',
   },
   { path: 'home', loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule) },
 ];
