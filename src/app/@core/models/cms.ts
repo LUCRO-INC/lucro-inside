25a0db29cbb3d0069a7d5ea54c73f0faab1ded29
Ivan D. Velez <ivancho175@hotmail.com>
2021-10-05 09:54:13 -0500

    Unifying components of 'Empresas Section'

--- src/app/@core/models/cms.ts
+++
@@ -1,5 +0,0 @@
-import { Section } from "./section";
-
-export interface Cms {
-    home?:Array<Section>
-}
--- src/app/@core/models/section.ts
+++
@@ -1,5 +0,0 @@
-export interface Section {
-    name?: String;
-    title?: String;
-    body?: String;
-}
