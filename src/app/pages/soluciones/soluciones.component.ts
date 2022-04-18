4789c8ffad1b6fa09bbd530ea2dee584f072c104
Ivan D. Velez <ivancho175@hotmail.com>
2022-04-18 10:18:34 -0500

    bug: working on 'Soluciones' broken scroll

--- src/app/pages/soluciones/soluciones.component.ts
+++ src/app/pages/soluciones/soluciones.component.ts
@@ -307,8 +307,11 @@ export class SolucionesComponent implements OnInit, AfterViewInit, OnDestroy {
       }
     })
     this.soluciones.forEach((s, idx) => {
+      this.sections[idx].nativeElement.classList.remove('active');
       if (s.label == solucionLabel) {
-        this.sections[idx].nativeElement.scrollIntoView({block: 'start'});
+        this.sections[idx].nativeElement.classList.add('active');
+        // s;
+        /* this.sections[idx].nativeElement.scrollIntoView({block: 'start'}); */
       }
     })
     // switch (solucionLabel) {
@@ -357,26 +360,13 @@ export class SolucionesComponent implements OnInit, AfterViewInit, OnDestroy {
     this.observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          let sectionTarget: any = entry.target;
-          let sectionLabel: string = sectionTarget.ariaLabel;
-          sectionTarget.classList.add('active');
-          sectionTarget.classList.add('show');
-          let navbarItems = document.querySelectorAll('.nav-button');
-          navbarItems.forEach((item: any) => {
-            let navbarLabel = item.innerText;
-            let activeTags: Array<Element> = [];
-            // if (item.classList.contains('highlighted')) {
-            //   item.classList.remove('highlighted');
-            //   item.parentElement.classList.remove('swiper-slide-active');
-            // }
-            if (navbarLabel === sectionLabel && !item.parentElement.classList.contains('swiper-slide-duplicate')) {
-              // activeTags.push(item);
-              // console.log(activeTags)
-              item.click();
-              // item.classList.add('highlighted');
-              // item.parentElement.classList.add('swiper-slide-active');
-            }
-          })
+           console.log(entry.target.classList)
+          entry.target.classList.add('show');
+          // let navbarItems = document.querySelectorAll('.nav-button');
+          // console.log(navbarItems)
+          // navbarItems.forEach(item => {
+          //   item.classList.remove('highlighted');
+          // })
           this.clickArrow = false;
         }
       });
